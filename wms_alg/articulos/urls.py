a41ed880b93d73016829f00bffa5d5936a66e18e
Jose Manuel Gomez <jmgomezr@gmail.com>
2018-12-17 16:21:54 +0100

    Views de articulos
    Empezando a montar las views, para poder editar contenido de artículos.

--- wms_alg/articulos/urls.py
+++ wms_alg/articulos/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /articulos
     url(r'^$', views.index, name='index'),
+    # ex: /articulos/5/
+    url(r'^(?P<articulo_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- wms_alg/articulos/views.py
+++ wms_alg/articulos/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Articulo
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the articulos index.")
+    first100 = Articulo.objects.order_by('descripcion')[:100]
+    template = loader.get_template('articulos/index.html')
+    context = {
+        'first100' : first100,
+    }
+    # output = ', '.join(art.descripcion for art in first100)
+    return HttpResponse(template.render(context, request))
+    # Listado de articulos paginados
+
+
+def detail(request, articulo_id):
+    return HttpResponse("You're looking at articulo %s." % articulo_id)
